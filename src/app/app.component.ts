becde95d6c1bd46b0c9f333f380fff761c6d354c
dockerlead <dockerlead@mail.com>
2021-03-15 23:54:31 -0500

    Use @ViewChild decorator for accessing form data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import {NgForm} from '@angular/forms';
 
 @Component({
@@ -7,11 +7,13 @@ import {NgForm} from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('formEl') signupForm: NgForm;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
 
   onSubmit(formEl: NgForm) {
-    console.log(formEl.form.value)
+    console.log(this.signupForm)
   }
 }
